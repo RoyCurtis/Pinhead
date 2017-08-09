937d095c0ca51684899162f98cbb99034a970d87
Roy Curtis <roy.adrian.curtis+github@gmail.com>
2017-08-09 17:32:14 +0100

    Implement role check

--- index.js
+++ index.js
@@ -4,23 +4,52 @@
  */
 
 /* Imports */
+const Meta    = require("./package.json");
 const Discord = require("discord.js");
 const YAML    = require('js-yaml');
 const FS      = require('fs');
+const Util    = require('util.js');
 
 /* Globals */
-const config = YAML.safeLoad( FS.readFileSync('config.yml', 'utf8') );
+console.log(`*** Starting Pinhead ${Meta.version} ***`);
+
+const config = YAML.safeLoad( FS.readFileSync('./config.yml', 'utf8') );
 const client = new Discord.Client();
 
-/* Main logic */
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-});
+/* Event handlers */
 
-client.on('message', msg => {
-    if (msg.content === 'ping') {
-    msg.reply('Pong!');
+// Handles initial connection to discord
+function onReady()
+{
+    console.log(`Logged in as ${client.user.tag}`);
 }
-});
 
-client.login('token');
+/**
+ *
+ * @param {MessageReaction} react
+ * @param {User} user
+ */
+function onReaction(react, user)
+{
+    const message = react.message;
+    const channel = message.channel;
+
+    // Ignore if this happened in the target channel
+    if ( channel.name.toLowerCase() === config.pinner.channel.toLowerCase() )
+        return;
+
+    // Ignore if this user is not permitted to pin
+    if ( !Util.hasRole(user, message.guild, config.pinner.role) )
+    {
+        console.debug(`${user.tag} not permitted to use superpin; ignoring`);
+        return;
+    }
+    else
+        console.debug("Skipping role check due to blank role in config");
+}
+
+// Finally, attach handlers and begin operation
+client.on('ready', onReady);
+client.on('messageReactionAdd', onReaction);
+client.login(config.botToken);
+
---
+++ util.js
@@ -0,0 +1,40 @@
+/*
+ * Pinhead, the pinning bot
+ * Roy Curtis, 2017, MIT license
+ */
+
+const exports = {};
+
+/**
+ * Checks if a given user has the given role in the given guild
+ *
+ * @param {User} user
+ * @param {Guild} guild
+ * @param {string} roleSearch
+ */
+exports.hasRole = function(user, guild, roleSearch)
+{
+    // If given role is empty, assume it's a match-all
+    if (!roleSearch || roleSearch.trim() === "")
+        return true;
+
+    /** @type {Role} */
+    let role   = null;
+    /** @type {GuildMember} */
+    let member = null;
+
+    for ( role of guild.roles.values() )
+    {
+        // Skip non-matching roles
+        if (role.name.toLowerCase() !== roleSearch)
+            continue;
+
+        for ( member of role.members.values() )
+            if (member.user === user) return true;
+    }
+
+    // No match found
+    return false;
+};
+
+module.exports = exports;
